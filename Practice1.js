211f1d677fbbf4e3dd7442b7db19e329b73dda69
AtikulRahi <82967842+AtikulRahi@users.noreply.github.com>
2024-02-25 00:37:06 +0600

    Uploaded the first practice

---
+++ Practice1.js
@@ -0,0 +1,24 @@
+var firstName= "Atikul";
+var secondName= " Islam";
+var fullName= firstName + secondName;
+console.log(fullName);
+
+var num=5;
+var str= "60";
+var sum= num + Number(str);
+console.log(sum);
+
+var Num1= 59;
+var txt1= "hello JS";
+var compare= Num1==txt1;
+console.log(compare);
+
+var Num2= 344;
+var txtNum= "344";
+var compare= Num2==txtNum;
+console.log(compare);
+
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
